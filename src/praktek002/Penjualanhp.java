f8defff8c4f78f217e14599464576953c8861b18
eddyyucca <eddyyucca@gmail.com>
2018-03-29 21:21:20 +0800

    membuat attribut dan behavior

--- src/praktek002/Penjualanhp.java
+++ src/praktek002/Penjualanhp.java
@@ -2,5 +2,13 @@ package praktek002;
 
 
 public class Penjualanhp {
+    Integer hargahp;
+    Integer jumlahbarang;
     
+    cetakInfo(){
+        System.out.println("=======================");
+        System.out.println("harga hp : "+hargahp);
+        System.out.println("jumlah barang : "+jumlahbarang);
+        System.out.println("=======================");
+    }
 }
