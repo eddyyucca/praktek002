bca37b31b7826a05bc6ac94af64621cd4a770cc3
eddyyucca <eddyyucca@gmail.com>
2018-03-29 21:41:38 +0800

    mengisi nilai attribut

--- src/praktek002/penjualanhpaksi.java
+++ src/praktek002/penjualanhpaksi.java
@@ -5,6 +5,10 @@ public class penjualanhpaksi {
     public static void main(String[] args) {
         Penjualanhp r1 = new Penjualanhp();
         
+        
+       r1.hargahp = 1000000;
+       r1.jumlahbarang = 10;
+       
     }
   
 }
